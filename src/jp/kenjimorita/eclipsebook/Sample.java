a7d46ba4a1f22b3ad37a1591198d08eb6c5b0577
kenji <kennjimoritata@yahoo.co.jp>
2016-07-10 10:56:07 +0900

    auto boxing, etc

--- src/jp/kenjimorita/eclipsebook/Sample.java
+++ src/jp/kenjimorita/eclipsebook/Sample.java
@@ -4,6 +4,17 @@ package jp.kenjimorita.eclipsebook;
 public class Sample {
 	public static void main(String[] args) {
 		calc("134");
+		printAll("ee", "eee");
+		String[] s = {"Wellcom", "to", "Java", "world"};
+		printAll(s);
+		
+		Integer obj = 10;
+		int n = obj;//アンボクシング 
+	}
+	public static void printAll(String... arg){//変数引数(個数不定の引数をサポート)
+		for(String s : arg){
+			System.out.println(s);
+		}
 	}
 	public static void calc(String str){
 		int n;
